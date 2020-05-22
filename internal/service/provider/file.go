c236dd8150c89b33eb5348b87ae0e960064f09db
Diego Bernardes <diego.bernardes@gonitro.com>
2020-05-22 10:01:25 +0100

    Refactor file provider

--- internal/service/provider/file.go
+++ internal/service/provider/file.go
@@ -5,7 +5,6 @@ import (
 	"context"
 	"errors"
 	"fmt"
-	"io"
 	"io/ioutil"
 	"net/url"
 	"os"
@@ -15,49 +14,40 @@ import (
 	"github.com/PuerkitoBio/goquery"
 )
 
+type fileReader interface {
+	fileExists(string) (os.FileInfo, bool)
+	readFile(string) ([]byte, error)
+}
+
 type fileParser interface {
 	Do(payload []byte) []byte
 	SanitizedAnchorName(text string) string
 }
 
-// fileHelpersC contain the implemenation of helpers for providers
-type fileHelpersC struct{}
+type fileReaderAPI struct{}
 
-// File provider is responsible for checking if the file exists at the filesystem.
-type File struct {
-	Path    string
-	Parser  fileParser
-	Helpers FileHelpers
-
-	schemaRegex regexp.Regexp
+func (fileReaderAPI) fileExists(item string) (os.FileInfo, bool) {
+	info, err := os.Stat(item)
+	return info, !os.IsNotExist(err)
 }
 
-// Helpers
-func (h fileHelpersC) fileExists(item string) (os.FileInfo, bool) {
-	finfo, err := os.Stat(item)
-	if os.IsNotExist(err) {
-		return finfo, false
-	}
-	return finfo, true
-}
-
-func (h fileHelpersC) regexCompile(str string) (*regexp.Regexp, error) {
-	return regexp.Compile(str)
-}
-
-func (h fileHelpersC) readFile(filer string) ([]byte, error) {
+func (fileReaderAPI) readFile(filer string) ([]byte, error) {
 	return ioutil.ReadFile(filer)
 }
 
-func (h fileHelpersC) docQuery(r io.Reader) (*goquery.Document, error) {
-	return goquery.NewDocumentFromReader(r)
+// File provider is responsible for checking if the file exists at the filesystem.
+type File struct {
+	Path   string
+	Parser fileParser
+
+	reader      fileReader
+	schemaRegex regexp.Regexp
 }
 
 // Init internal state.
 func (f *File) Init() error {
-	// If not Helpers assigned: use default
-	if f.Helpers == nil {
-		f.Helpers = fileHelpersC{}
+	if f.reader == nil {
+		f.reader = fileReaderAPI{}
 	}
 
 	if f.Path == "" {
@@ -82,22 +72,22 @@ func (f File) Authority(uri string) bool {
 
 // Valid check if the link is valid.
 func (f File) Valid(ctx context.Context, filePath, uri string) (bool, error) {
-	found, err := f.checkMarkdown(filePath, uri)
-	if err != nil {
-		return false, fmt.Errorf("fail to check the markdown: %w", err)
-	}
-	if found {
-		return true, nil
+	if f.isMarkdown(filePath) {
+		found, err := f.checkMarkdown(filePath, uri)
+		if err != nil {
+			return false, fmt.Errorf("fail to check the markdown: %w", err)
+		}
+		return found, nil
 	}
 
 	path := filepath.Join(filepath.Dir(filePath), uri)
-	_, itemExists := f.Helpers.fileExists(path)
+	_, itemExists := f.reader.fileExists(path)
 	return itemExists, nil
 }
 
 func (f *File) initRegex() error {
 	expr := "^.*$"
-	schema, err := f.Helpers.regexCompile(expr)
+	schema, err := regexp.Compile(expr)
 	if err != nil {
 		return fmt.Errorf("fail to compile the expression '%s': %w", expr, err)
 	}
@@ -105,13 +95,12 @@ func (f *File) initRegex() error {
 	return nil
 }
 
+func (File) isMarkdown(path string) bool {
+	return filepath.Ext(path) == ".md"
+}
+
 // checkMarkdown check if the uri is a Markdown, if positive, it will be responsible to detect if the link is valid.
 func (f File) checkMarkdown(path, uri string) (bool, error) {
-	mdExtension := ".md"
-	if filepath.Ext(path) != mdExtension {
-		return false, nil
-	}
-
 	parsedURI, err := url.Parse(uri)
 	if err != nil {
 		return false, fmt.Errorf("fail to parse the uri '%s': %w", uri, err)
@@ -128,27 +117,25 @@ func (f File) checkMarkdown(path, uri string) (bool, error) {
 
 	// Check if the path exists, if not, it will return to the fallback verification at the caller.
 	// If the path is a directory we get a valid response.
-	pathStat, valid := f.Helpers.fileExists(expandedPath)
-
+	pathStat, valid := f.reader.fileExists(expandedPath)
 	if !valid {
 		return false, nil
 	}
-
 	if pathStat.IsDir() {
 		return true, nil
 	}
 
-	if filepath.Ext(expandedPath) != mdExtension {
+	if filepath.Ext(expandedPath) != ".md" {
 		return true, nil
 	}
 
-	payload, err := f.Helpers.readFile(expandedPath)
+	payload, err := f.reader.readFile(expandedPath)
 	if err != nil {
 		return false, fmt.Errorf("fail to read the file '%s': %w", expandedPath, err)
 	}
 	payload = f.Parser.Do(payload)
 
-	doc, err := f.Helpers.docQuery(bytes.NewBuffer(payload))
+	doc, err := goquery.NewDocumentFromReader(bytes.NewBuffer(payload))
 	if err != nil {
 		return false, fmt.Errorf("fail to parse the HTML: %w", err)
 	}
